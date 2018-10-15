2c4b24e4a36ad4fc791942f01adea3ffefcd4f80
Gabriel Seijo <gabrielseijo.m@gmail.com>
2018-10-15 23:20:43 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -12,8 +12,8 @@ def summon_captain_planet(calls)
 end
 
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(calls)
+  if calls.any?
 end
 
 def find_the_cheese# code an argument here
